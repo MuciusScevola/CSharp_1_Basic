92ca7e797d32f96b7ccc5f7ff9a6614c8eec5fb5
Ilya Par <ilya-par@yandex.ru>
2025-03-14 23:19:24 +0300

    Очередная попытка.

---
+++ Theme02_Tasks/Theme02_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Theme02_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a1, a2, a3;
+            a1 = a2 = a3 = Convert.ToInt32(Console.ReadLine());
+            
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Исходные значения: {a1},{a2},{a3}");
+            a1 = a2;
+            a2 = a3;
+            a3 = a1;
+            
+            Console.WriteLine($"Переприсвоенные значения: {a1},{a2},{a3}");*/
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}

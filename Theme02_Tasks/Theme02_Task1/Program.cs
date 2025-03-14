c999a8a639b1b854ca19279794ae80e54e3dad95
Ilya Par <ilya-par@yandex.ru>
2025-03-14 23:22:41 +0300

    Вроде успешная попытка.

--- Theme02_Tasks/Theme02_Task1/Program.cs
+++ Theme02_Tasks/Theme02_Task1/Program.cs
@@ -4,16 +4,14 @@
     {
         static void Main(string[] args)
         {
-            /*int a1, a2, a3;
+            int a1, a2, a3;
             a1 = a2 = a3 = Convert.ToInt32(Console.ReadLine());
-            
-            b = Convert.ToInt32(Console.ReadLine());
-            c = Convert.ToInt32(Console.ReadLine());
+            /*b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());*/
             Console.WriteLine($"Исходные значения: {a1},{a2},{a3}");
             a1 = a2;
             a2 = a3;
             a3 = a1;
-            
             Console.WriteLine($"Переприсвоенные значения: {a1},{a2},{a3}");*/
             Console.WriteLine("Нажмите любую клавишу.");
             Console.ReadKey();

c6ed764e7b38ff4a4c758e9620611af3152426de
Ilya Par <ilya-par@yandex.ru>
2025-03-24 21:50:28 +0300

    Начал задачу 11.

--- Theme03_Tasks/Theme03_Task11/Program.cs
+++ Theme03_Tasks/Theme03_Task11/Program.cs
@@ -1,13 +1,43 @@
-﻿namespace Theme03_Task11
+﻿using System.ComponentModel;
+
+namespace Theme03_Task11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double nmr1, nmr2, nmr3;
-            nmr1 = Convert.ToDouble(Console.ReadLine());
-            nmr2 = Convert.ToDouble(Console.ReadLine());
-            nmr3 = Convert.ToDouble(Console.ReadLine());
+            double a, b, p, q, r, s;
+            Console.Write("Введите длину участка: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину участка: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину первого дома: ");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину первого дома: ");
+            q = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину второго дома: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину второго дома: ");
+            s = Convert.ToDouble(Console.ReadLine());
+
+            if (p + r >= a)
+            {
+                if (q + s >= b) {
+                    {
+                    Console.WriteLine("Да");
+                }
+            }
+            else if
+            {
+                Console.WriteLine("Да.");
+            }
+            else
+            {
+                Console.WriteLine("Нет.");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
         }
     }
 }

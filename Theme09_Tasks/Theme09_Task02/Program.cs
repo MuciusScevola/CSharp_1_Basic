ba83030db33b13f45c8003a82fa6f345f85f95ae
Ilya Par <ilya-par@yandex.ru>
2025-04-03 22:25:46 +0300

    Создание проектов под задачи.

---
+++ Theme09_Tasks/Theme09_Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Theme09_Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

6bf23b0bbcd6e173ead7ad20d307a91502bf40be
Ilya Par <ilya-par@yandex.ru>
2025-04-15 21:37:00 +0300

    Решение задачи 2.

---
+++ Theme06_Tasks/Theme06_Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Theme06_Task02
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.
+            //Составить программу, определяющую, является ли строка палиндромом без учёта пробелов и регистра.
+            Console.WriteLine("Введите предложение:");
+            string input = Console.ReadLine();
+
+            string sentenceAnalised = input.Replace(" ", "").ToLower(); //Приведение предложения к форме, которую можно анализировать.
+
+            bool isPalindrome = true;
+            int left = 0;
+            int right = sentenceAnalised.Length - 1;
+            while (left < right)
+            {
+                if (sentenceAnalised[left] != sentenceAnalised[right])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+
+            if (isPalindrome)
+                Console.WriteLine("Данное предложение палиндром.");
+            else
+                Console.WriteLine("Данное предложение не палиндром.");
+
+            Console.WriteLine("\nНажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Theme06_Tasks/Theme06_Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Theme06_Task04
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Запросите у пользователя имя, возраст и город. Сформируйте строку в формате:
+            //"Имя: [имя], Возраст: [возраст], Город: [город]" с использованием StringBuilder.
+
+
+        }
+    }
+}

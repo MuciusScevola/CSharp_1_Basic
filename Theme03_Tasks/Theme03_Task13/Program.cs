8494c8fdf1c0a8adc09b5f9892633e2957770447
Ilya Par <ilya-par@yandex.ru>
2025-04-23 18:34:37 +0300

    Исправления.

---
+++ Theme03_Tasks/Theme03_Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Theme03_Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

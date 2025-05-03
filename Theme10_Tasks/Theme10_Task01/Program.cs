350780a005de7d324f07aff9a94ea22fdc8c25bd
Ilya Par <ilya-par@yandex.ru>
2025-05-03 23:52:06 +0300

    Создание ветки по задаче 10.

---
+++ Theme10_Tasks/Theme10_Task01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Theme10_Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создайте базовый класс Building(описывает здание) со следующими характеристиками:
+
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+        public class Building
+        {
+            string _address;
+            double _area;
+            int _yearBuilt;
+
+            public Building(string address, double area, int yearBuilt)
+            {
+                _address = address;
+                _area = area;
+                _yearBuilt = yearBuilt;
+            }
+            public virtual decimal CalculateTax()
+            {
+                tax = area * 1000;
+                return tax;
+            }
+            public virtual void DisplayInfo()
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}

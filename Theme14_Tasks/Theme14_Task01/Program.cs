0bd51c68dfaf7dabb4aad5956dd8d9106f5e4ece
User <ilya-par@yandex.ru>
2025-06-23 22:32:03 +0300

    Решение задач по теме 14.

--- Theme14_Tasks/Theme14_Task01/Program.cs
+++ Theme14_Tasks/Theme14_Task01/Program.cs
@@ -1,6 +1,4 @@
-﻿namespace Theme14_Task01
-{
-// Разработайте программу для управления списком игроков команды, используя стандартные методы класса List<T>.
+﻿// Разработайте программу для управления списком игроков команды, используя стандартные методы класса List<T>.
 // Требования:
 //  Создайте список для хранения фамилий игроков(тип string).
 //  Реализуйте следующие операции:
@@ -15,11 +13,45 @@
 //     2. Козлов
 //     3. Петров
 //   Проверьте, пуст ли список (свойство Count), и очистите его методом Clear.
+
+namespace Theme14_Task01
+{
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            List<string> team = new List<string>();
+
+            team.Add("Иванов");
+            team.Add("Петров");
+            team.Add("Сидоров");
+
+            team.Insert(1, "Козлов");
+
+            bool isPetrov = team.Contains("Петров");
+            Console.WriteLine($"Есть ли игрок \"Петров\" в команде? => {isPetrov}.");
+
+            team.Remove("Сидоров");
+
+            int playerKozlov = team.IndexOf("Козлов");
+            Console.WriteLine($"\nИндекс игрока \"Козлов\": {playerKozlov}.");
+
+            team.Sort();
+
+            Console.WriteLine("\nТекущий состав команды:");
+            for (int i = 0; i < team.Count; i++)
+                Console.WriteLine($"  {i + 1}. {team[i]}.");
+
+            bool isTeamEmpty = team.Count == 0;
+            Console.WriteLine($"\nКоманда пуста? => {isTeamEmpty}.");
+
+            team.Clear();
+
+            // Выводим количество игроков после очистки
+            Console.WriteLine($"\nКоличество игроков после очистки: {team.Count}.");
+
+            Console.WriteLine("\nНажмите любую клавишу.");
+            Console.ReadKey();
         }
     }
 }
